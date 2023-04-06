128c5117ac186be86674b62338f7e977a8866bcb
JoasChoque <joaschoque@hotmail.com>
2023-04-06 17:19:18 -0300

    Corrigindo functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 let nome = window.document.getElementById("nome");
+let email = document.getElementById("email");
 let artista = document.getElementById("artista");
 let musica = document.getElementById("musica");
-let mensagem = document.getElementById('mensagem')
+let mensagem = document.getElementById("mensagem");
 
-let vNome=false;
-let vArtista=false;
-let vMusica=false;
+let vNome = false;
+let vArtista = false;
+let vMusica = false;
 let vMensagem = false;
+let vEmail = false;
+
 function validaNome() {
   let txtNome = document.querySelector("#txtNome");
 
@@ -14,10 +17,24 @@ function validaNome() {
     txtNome.innerHTML = "O nome deve conter no mínimo 3 caracteres!";
     txtNome.style.color = "#f6f1f1";
     txtNome.style.padding = "5px 0 0 0";
-    txtNome.style.display='block';
+    txtNome.style.display = "block";
   } else {
-    txtNome.style.display='none';
-    vNome=true;
+    txtNome.style.display = "none";
+    vNome = true;
+  }
+}
+
+function validaEmail() {
+  let txtEmail = document.querySelector("#txtEmail");
+
+  if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+    txtEmail.innerHTML = "O email deve ser um email válido";
+    txtEmail.style.color = "#f6f1f1";
+    txtEmail.style.padding = "5px 0 0 0";
+    txtEmail.style.display = "block";
+  } else {
+    txtEmail.style.display = "none";
+    vEmail = true;
   }
 }
 
@@ -29,8 +46,8 @@ function validaArtista() {
     txtArtista.innerHTML = "O nome do Artista deve conter no mínimo 2 caracteres";
     txtArtista.style.padding = "5px 0 0 0";
   } else {
-    txtArtista.style.display='none';
-    vArtista=true;
+    txtArtista.style.display = "none";
+    vArtista = true;
   }
 }
 
@@ -40,31 +57,29 @@ function validaMusica() {
 
   if (musica.value.length <= 1) {
     txtMusica.innerHTML = "O nome da música deve conter no mínimo 1 caracter";
-  }
-  else{ 
-    txtMusica.style.display='none';
-    vMusica=true;
+  } else {
+    txtMusica.style.display = "none";
+    vMusica = true;
   }
 }
 
-function tamanhoArea(){
-  let txtMensagem = document.querySelector('#txtMensagem')
+function tamanhoArea() {
+  let txtMensagem = document.querySelector("#txtMensagem");
   txtMensagem.style.color = "#f6f1f1";
 
-  if(mensagem.value.length>=100){
-    txtMensagem.innerHTML='O tamanho máximo é de 100 caracteres'
-  }
-  else{
-    txtMensagem.style.display='none';
-    vMensagem=true;
+  if (mensagem.value.length >= 100) {
+    txtMensagem.innerHTML = "O tamanho máximo é de 100 caracteres";
+    vMensagem = false;
+  } else {
+    txtMensagem.style.display = "none";
+    vMensagem = true;
   }
 }
 
-function enviar(){
-  if(vNome== true && vArtista== true && vMensagem== true && vMusica == true){
-    alert('Formulário Recebido! Obrigado pela recomendação! :D')
-  }
-  else{
-    alert('Por favor, preencha o formulário corretamente!')
+function enviar() {
+  if (vNome == true && vArtista == true && vMensagem == true && vMusica == true) {
+    alert("Formulário Recebido! Obrigado pela recomendação! :D");
+  } else {
+    alert("Por favor, preencha o formulário corretamente!");
   }
 }
